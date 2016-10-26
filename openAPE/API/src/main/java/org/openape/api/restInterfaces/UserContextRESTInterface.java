b368eaf37d153dcac6027f0562e5b45d0b89bf1d
su017 <su017@hdm-stuttgart.de>
2016-10-26 18:20:11 +0200

    Feature: adding basic code and support for java 1.8 in maven

---
+++ openAPE/API/src/main/java/org/openape/api/restInterfaces/UserContextRESTInterface.java
@@ -0,0 +1,20 @@
+package org.openape.api.restInterfaces;
+
+import org.openape.api.usercontext.UserContext;
+import org.openape.server.UserContextRequestHandler;
+
+import static spark.Spark.*;
+import spark.Spark;
+import spark.Request;
+import spark.Route;
+import spark.Response;
+
+public class UserContextRESTInterface {
+
+    public UserContextRESTInterface(
+            final UserContextRequestHandler requestHandler) {
+        get("/hello", (req, res) -> "Hello World");
+        // get("/api/user-context/:user-context-id", (req, res) ->
+        // requestHandler.);
+    }
+}
---
+++ openAPE/server/src/main/java/org/openape/server/UserContextRequestHandler.java
@@ -0,0 +1,9 @@
+package org.openape.server;
+
+import org.openape.api.usercontext.UserContext;
+
+public class UserContextRequestHandler {
+    public UserContext getUserContextById(String id) {
+        return null;
+    }
+}
