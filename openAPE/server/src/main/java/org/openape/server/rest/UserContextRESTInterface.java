e18b3073041849844433e042f2eeae59e08f36be
su017 <su017@hdm-stuttgart.de>
2016-11-01 23:11:58 +0100

    fix: removing unused inputs and update time table

--- openAPE/server/src/main/java/org/openape/server/rest/UserContextRESTInterface.java
+++ openAPE/server/src/main/java/org/openape/server/rest/UserContextRESTInterface.java
@@ -1,13 +1,8 @@
 package org.openape.server.rest;
 
-import org.openape.api.usercontext.UserContext;
 import org.openape.server.UserContextRequestHandler;
 
 import static spark.Spark.*;
-import spark.Spark;
-import spark.Request;
-import spark.Route;
-import spark.Response;
 
 public class UserContextRESTInterface {
 
