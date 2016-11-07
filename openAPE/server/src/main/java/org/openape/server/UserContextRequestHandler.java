29494e91be11c8d6072d2da7007e067344bb54db
Manuel Bidmon <manuel.bidmon@yahoo.de>
2016-11-07 15:58:59 +0100

    Feature: added requests for user context
    Added raw post, put, delete requests

--- openAPE/server/src/main/java/org/openape/server/UserContextRequestHandler.java
+++ openAPE/server/src/main/java/org/openape/server/UserContextRequestHandler.java
@@ -3,7 +3,19 @@ package org.openape.server;
 import org.openape.api.usercontext.UserContext;
 
 public class UserContextRequestHandler {
+    public void createUserContext(Object useContext){
+    	
+    }
+    
     public UserContext getUserContextById(String id) {
         return null;
+    }
+    
+    public void updateUserContextById(String id, Object useContext){
+    	
+    }
+    
+    public void deleteUserContextById(String id){
+    	
     }
 }
--- openAPE/server/src/main/java/org/openape/server/rest/UserContextRESTInterface.java
+++ openAPE/server/src/main/java/org/openape/server/rest/UserContextRESTInterface.java
@@ -16,6 +16,15 @@ public class UserContextRESTInterface {
          * http://localhost:8080/hello
          */
         get("/hello", (req, res) -> "Hello World");
+        
+        /**
+         * Request 7.2.2 create user-context.
+         */
+        post("/api/user-context/", (req, res) -> {
+            Object useContext = req.params(":user-context");
+            requestHandler.createUserContext(useContext);
+            return null;
+        });
 
         /**
          * Request 7.2.3 get user-context. Used to get a specific user context
@@ -25,6 +34,25 @@ public class UserContextRESTInterface {
             String userContextId = req.params(":user-context-id");
             res.type("application/json");
             return requestHandler.getUserContextById(userContextId);
+        });
+        
+        /**
+         * Request 7.2.4 update user-context.
+         */
+        put("/api/user-context/user-context-id", (req, res) -> {
+            String userContextId = req.params(":user-context-id");
+            Object useContext = req.params(":user-context");
+            requestHandler.updateUserContextById(userContextId, useContext);
+            return null;
+        });
+        
+        /**
+         * Request 7.2.5 delete user-context.
+         */
+        delete("/api/user-context/user-context-id", (req, res) -> {
+            String userContextId = req.params(":user-context-id");
+            requestHandler.deleteUserContextById(userContextId);
+            return null;
         });
 
     }
