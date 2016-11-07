22ae62ac9e4f64f1d9406cc72570fb4b55eedc42
su017 <su017@hdm-stuttgart.de>
2016-11-07 22:05:00 +0100

    Fix: updating handler methods and adding first comment

--- openAPE/server/src/main/java/org/openape/server/UserContextRequestHandler.java
+++ openAPE/server/src/main/java/org/openape/server/UserContextRequestHandler.java
@@ -1,21 +1,38 @@
 package org.openape.server;
 
+import java.io.IOException;
+
 import org.openape.api.usercontext.UserContext;
+import org.openape.server.rest.UserContextRESTInterface;
 
 public class UserContextRequestHandler {
-    public void createUserContext(Object useContext){
-    	
+
+    /**
+     * Method to store a new user context into the server. It is used by the
+     * rest API {@link UserContextRESTInterface} and uses the server database.
+     * 
+     * @param userContext
+     *            to be stored.
+     * @return the ID of the stored user context.
+     * @throws IOException
+     *             if a storage problem occurs-
+     * @throws IllegalArgumentException
+     *             if the parameter is not a complete user context.
+     */
+    public String createUserContext(Object userContext) throws IOException,
+            IllegalArgumentException {
+        return null;
     }
-    
+
     public UserContext getUserContextById(String id) {
         return null;
     }
-    
-    public void updateUserContextById(String id, Object useContext){
-    	
+
+    public boolean updateUserContextById(String id, Object useContext) {
+        return false;
     }
-    
-    public void deleteUserContextById(String id){
-    	
+
+    public boolean deleteUserContextById(String id) {
+        return false;
     }
 }
