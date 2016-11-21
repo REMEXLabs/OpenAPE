09213630e5e9d6d3f45ef30679ba1cce2874d861
Stephan Unfried <unfriesn@studi.informatik.uni-stuttgart.de>
2016-11-21 16:04:36 +0100

    Fix: adding mong db object dependency

--- openAPE/API/src/main/java/org/openape/api/DatabaseObject.java
+++ openAPE/API/src/main/java/org/openape/api/DatabaseObject.java
@@ -2,10 +2,12 @@ package org.openape.api;
 
 import java.io.Serializable;
 
+import com.mongodb.BasicDBObject;
+
 /**
  * Abstract class used to identify object types that can be stored in the
  * database.
  */
-public abstract class DatabaseObject implements Serializable {
+public abstract class DatabaseObject extends BasicDBObject implements Serializable {
     private static final long serialVersionUID = -404247926481623440L;
 }
