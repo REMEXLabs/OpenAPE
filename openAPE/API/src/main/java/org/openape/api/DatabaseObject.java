c73825500549e43a71d9f9f4963a3d90f07ee236
su017 <su017@hdm-stuttgart.de>
2016-12-03 19:56:22 +0100

    Fix: remove wrong usage of basicDBObject

--- openAPE/API/src/main/java/org/openape/api/DatabaseObject.java
+++ openAPE/API/src/main/java/org/openape/api/DatabaseObject.java
@@ -8,6 +8,6 @@ import com.mongodb.BasicDBObject;
  * Abstract class used to identify object types that can be stored in the
  * database.
  */
-public abstract class DatabaseObject extends BasicDBObject implements Serializable {
+public abstract class DatabaseObject implements Serializable {
     private static final long serialVersionUID = -404247926481623440L;
 }
