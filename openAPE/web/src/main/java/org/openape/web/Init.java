6300bb4fbdd6ebc4a976b2dbe9f5cb299473fc5e
su017 <su017@hdm-stuttgart.de>
2016-12-10 18:06:30 +0100

    Freature: adding server initialize code to init

--- openAPE/web/src/main/java/org/openape/web/Init.java
+++ openAPE/web/src/main/java/org/openape/web/Init.java
@@ -1,5 +1,28 @@
 package org.openape.web;
 
-public class Init {
+import org.openape.server.database.DatabaseConnection;
+import org.openape.server.requestHandler.EnvironmentContextRequestHandler;
+import org.openape.server.requestHandler.EquipmentContextRequestHandler;
+import org.openape.server.requestHandler.TaskContextRequestHandler;
+import org.openape.server.requestHandler.UserContextRequestHandler;
+import org.openape.server.rest.EnvironmentContextRESTInterface;
+import org.openape.server.rest.EquipmentContextRESTInterface;
+import org.openape.server.rest.TaskContextRESTInterface;
+import org.openape.server.rest.UserContextRESTInterface;
+
+import spark.servlet.SparkApplication;
+
+public class Init implements SparkApplication {
+
+    @Override
+    public void init() {
+        // Start rest api and database connection.
+        DatabaseConnection.getInstance();
+        new UserContextRESTInterface(new UserContextRequestHandler());
+        new EnvironmentContextRESTInterface(new EnvironmentContextRequestHandler());
+        new EquipmentContextRESTInterface(new EquipmentContextRequestHandler());
+        new TaskContextRESTInterface(new TaskContextRequestHandler());
+
+    }
 
 }
