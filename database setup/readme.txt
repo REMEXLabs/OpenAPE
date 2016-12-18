b7a22ce701f242886c16e68dfc44c6dd6c9add43
su017 <su017@hdm-stuttgart.de>
2016-12-18 17:43:19 +0100

    Feature: adding setup.js to repo

---
+++ database setup/readme.txt
@@ -0,0 +1,4 @@
+To use the setup.js:
+1. Change <serverAddress> and <password> to valide strings.
+2. Install mongo shell as stated here: https://docs.mongodb.com/v3.2/installation/
+3. Use the 'load("setup.js")' command while beeing in the folder containing the setup.js
---
+++ database setup/setup.js
@@ -0,0 +1,9 @@
+﻿var connection = new Mongo(<address>:27017);
+var database = conn.getDatabase("openAPE");
+database.addUser("openAPE", "<password>");
+database.createCollection("environment_contexts");
+database.createCollection("equipment_contexts");
+database.createCollection("resource_offers");
+database.createCollection("resource_requests");
+database.createCollection("task_contextss");
+database.createCollection("user_contexts");
