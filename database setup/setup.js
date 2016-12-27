ec64f2034251f98c59ffd4bd4d7f12bef050c3b1
su017 <su017@hdm-stuttgart.de>
2016-12-27 18:28:01 +0100

    fix: fix java script

--- database setup/setup.js
+++ database setup/setup.js
@@ -1,9 +1,14 @@
-﻿var connection = new Mongo(<serverAddress>:27017);
-var database = conn.getDatabase("openAPE");
-database.addUser("openAPE", "<password>");
+﻿var connection = new Mongo("<serverAddress>:27017");
+var database = connection.getDB("openAPE");
+database.createUser({
+     user: "openAPE",
+     pwd: "<userPassword>",
+     roles: [ "readWrite", "dbAdmin" ]});
 database.createCollection("environment_contexts");
 database.createCollection("equipment_contexts");
 database.createCollection("resource_offers");
 database.createCollection("resource_requests");
 database.createCollection("task_contextss");
 database.createCollection("user_contexts");
+database.createCollection("test");
+database.test.insert( { x: 1 } );
