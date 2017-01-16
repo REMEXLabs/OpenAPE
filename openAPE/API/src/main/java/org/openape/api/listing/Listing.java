871e2dd45902e991e95766b1b2c9bc7aa0d33e9b
su017 <su017@hdm-stuttgart.de>
2017-01-16 17:11:16 +0100

    Feature: add listing code

--- openAPE/API/src/main/java/org/openape/api/listing/Listing.java
+++ openAPE/API/src/main/java/org/openape/api/listing/Listing.java
@@ -1,12 +1,25 @@
 package org.openape.api.listing;
 
+import javax.xml.bind.annotation.XmlAttribute;
+
 import org.openape.api.DatabaseObject;
 
 public class Listing extends DatabaseObject {
     private static final long serialVersionUID = 6662310079884980939L;
 
+    private String query = "";
+
     @Override
     public boolean isValid() {
         return true;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    @XmlAttribute(name = "query")
+    public void setQuery(String query) {
+        this.query = query;
     }
 }
