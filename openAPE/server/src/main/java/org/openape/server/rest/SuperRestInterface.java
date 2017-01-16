709f60308f9cfae3027f1a10e950dbf0f4d298fc
su017 <su017@hdm-stuttgart.de>
2017-01-16 21:03:31 +0100

    fix: add response code 204

--- openAPE/server/src/main/java/org/openape/server/rest/SuperRestInterface.java
+++ openAPE/server/src/main/java/org/openape/server/rest/SuperRestInterface.java
@@ -13,6 +13,7 @@ public class SuperRestInterface {
 
     public static final int HTTP_STATUS_OK = 200;
     public static final int HTTP_STATUS_CREATED = 201;
+    public static final int HTTP_STATUS_NO_CONTENT = 204;
     public static final int HTTP_STATUS_BAD_REQUEST = 400;
     public static final int HTTP_STATUS_NOT_FOUND = 404;
     public static final int HTTP_STATUS_INTERNAL_SERVER_ERROR = 500;
