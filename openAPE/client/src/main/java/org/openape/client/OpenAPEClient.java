fd90e757290ce3e0213598b6ec8e36ffde074ee9
Lukas84 <smirek@hdm-stuttgart.de>
2017-01-18 11:57:13 +0100

    #37 Create client

---
+++ openAPE/client/src/main/java/org/openape/client/OpenAPEClient.java
@@ -0,0 +1,81 @@
+package org.openape.client;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+
+
+
+public class OpenAPEClient {
+private Client client;
+private WebTarget webResource;
+
+public OpenAPEClient(String uri) {
+	ClientConfig config = new ClientConfig();
+	 client = ClientBuilder.newClient(config);
+webResource = client.target(uri);
+}
+	
+public URI createUserContext(){
+	Invocation.Builder invocationBuilder = webResource.request();
+
+return null;
+}
+
+
+	public void getListing(String url) {
+		Invocation.Builder invocationBuilder = webResource.request();
+
+
+			 
+				
+
+	}
+	
+	
+	public File getResource(URI uri, String targetFile){
+		
+        Invocation.Builder invocationBuilder = webResource.request();
+
+Response response = invocationBuilder.get();
+
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+			}
+			
+			InputStream in = (InputStream) response.readEntity(InputStream.class);
+//			
+			File tf = new File(targetFile);
+					try {
+						Files.copy(in, tf.toPath());
+		                 in.close();
+
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						return null;
+					}
+
+
+		
+		return tf;
+	}
+
+	public File getResource(String url, String targetFile) throws URISyntaxException {
+		URI uri = new URI(url);
+return 		getResource(uri, targetFile);
+		
+	}
+	
+}
