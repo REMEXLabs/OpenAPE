149858f9096e1f977eff16efcb7f6879dd0c07d0
su017 <su017@hdm-stuttgart.de>
2017-01-20 15:08:35 +0100

    fix: update collection names

--- database setup/setup.js
+++ database setup/setup.js
@@ -6,8 +6,8 @@ database.createUser({
      roles: [ "readWrite", "dbAdmin" ]});
 database.createCollection("environment_contexts");
 database.createCollection("equipment_contexts");
-database.createCollection("resource_description");
-database.createCollection("listing");
+database.createCollection("resource_descriptions");
+database.createCollection("listings");
 database.createCollection("task_contexts");
 database.createCollection("user_contexts");
 database.createCollection("test");
