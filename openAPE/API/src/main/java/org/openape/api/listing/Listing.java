c7da4e35ac47490c5c8d7ba54d84922cbb82f0e9
su017 <su017@hdm-stuttgart.de>
2017-02-21 21:44:58 +0100

    fix: update listing acording to standard

--- openAPE/API/src/main/java/org/openape/api/listing/Listing.java
+++ openAPE/API/src/main/java/org/openape/api/listing/Listing.java
@@ -1,17 +1,51 @@
 package org.openape.api.listing;
 
-import javax.xml.bind.annotation.XmlAttribute;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.openape.api.DatabaseObject;
+import org.openape.api.resourceDescription.ResourceDescription;
 
+/**
+ * Listing object defined in A.8.2
+ */
+@XmlRootElement
 public class Listing extends DatabaseObject {
     private static final long serialVersionUID = 6662310079884980939L;
 
-    private String query = "";
+    List<String> userContextUris = new ArrayList<String>();
+    List<String> taskContextUris = new ArrayList<String>();
+    List<String> equipmentContextUris = new ArrayList<String>();
+    List<String> environmantContextUris = new ArrayList<String>();
+    ResourceDescription resourceDescriptionQurey = new ResourceDescription();
 
-    public String getQuery() {
-        return this.query;
+    @XmlElement(name = "environment-context-uri")
+    public List<String> getEnvironmantContextUris() {
+        return this.environmantContextUris;
+    }
+
+    @XmlElement(name = "equipment-context-uri")
+    public List<String> getEquipmentContextUris() {
+        return this.equipmentContextUris;
+    }
+
+    @XmlElement(name = "resource-description")
+    public ResourceDescription getResourceDescriptionQurey() {
+        return this.resourceDescriptionQurey;
+    }
+
+    @XmlElement(name = "task-context-uri")
+    public List<String> getTaskContextUris() {
+        return this.taskContextUris;
+    }
+
+    @XmlElement(name = "user-context-uri")
+    public List<String> getUserContextUris() {
+        return this.userContextUris;
     }
 
     @Override
@@ -20,8 +54,23 @@ public class Listing extends DatabaseObject {
         return true;
     }
 
-    @XmlAttribute(name = "query")
-    public void setQuery(String query) {
-        this.query = query;
+    public void setEnvironmantContextUris(List<String> environmantContextUris) {
+        this.environmantContextUris = environmantContextUris;
+    }
+
+    public void setEquipmentContextUris(List<String> equipmentContextUris) {
+        this.equipmentContextUris = equipmentContextUris;
+    }
+
+    public void setResourceDescriptionQurey(ResourceDescription resourceDescriptionQurey) {
+        this.resourceDescriptionQurey = resourceDescriptionQurey;
+    }
+
+    public void setTaskContextUris(List<String> taskContextUris) {
+        this.taskContextUris = taskContextUris;
+    }
+
+    public void setUserContextUris(List<String> userContextUris) {
+        this.userContextUris = userContextUris;
     }
 }
