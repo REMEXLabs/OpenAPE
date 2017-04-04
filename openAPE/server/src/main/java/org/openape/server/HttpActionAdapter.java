87f9a6317ed11dc72c5ffa20da916e29d49adfed
Benjamin Bestmann <b.bestmann@gmail.com>
2017-04-04 16:51:48 +0200

    Implement HttpActionAdapter
    Add an implementation of DefaultHttpActionAdapter in order to respond with proper HTTP response codes on auth errors.

---
+++ openAPE/server/src/main/java/org/openape/server/HttpActionAdapter.java
@@ -0,0 +1,22 @@
+package org.openape.server;
+
+import org.pac4j.core.context.HttpConstants;
+import org.pac4j.sparkjava.DefaultHttpActionAdapter;
+import org.pac4j.sparkjava.SparkWebContext;
+import static spark.Spark.halt;
+
+public class HttpActionAdapter extends DefaultHttpActionAdapter {
+
+    @Override
+    public Object adapt(int code, SparkWebContext context) {
+        if (code == HttpConstants.UNAUTHORIZED) {
+            halt(401, "Unauthorized");
+        } else if (code == HttpConstants.FORBIDDEN) {
+            halt(403, "Forbidden");
+        } else {
+            return super.adapt(code, context);
+        }
+        return null;
+    }
+
+}
