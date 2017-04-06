d8d49c23f55a76592e9a8440af2dbb0705e4c88a
Benjamin Bestmann <b.bestmann@gmail.com>
2017-04-06 19:10:14 +0200

    Create user collection on database setup
    Extend the database setup script to also create a user collection called „users“.

--- Setup database and server/setup mongoDB/setup.js
+++ Setup database and server/setup mongoDB/setup.js
@@ -11,5 +11,6 @@ database.createCollection("listings");
 database.createCollection("task_contexts");
 database.createCollection("user_contexts");
 database.createCollection("resources");
+database.createCollection("users");
 database.createCollection("test");
 database.test.insert( { x: 1 } );
