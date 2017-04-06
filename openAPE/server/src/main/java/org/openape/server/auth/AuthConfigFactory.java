8680ac793086d398caab229cbe6b6624e036b1f9
Benjamin Bestmann <b.bestmann@gmail.com>
2017-04-06 18:12:03 +0200

    Switch to header authorization
    Authorize requests based on the HTTP Authorization header instead of URL parameter.

--- openAPE/server/src/main/java/org/openape/server/auth/AuthConfigFactory.java
+++ openAPE/server/src/main/java/org/openape/server/auth/AuthConfigFactory.java
@@ -3,7 +3,7 @@ package org.openape.server.auth;
 import org.pac4j.core.authorization.authorizer.RequireAnyRoleAuthorizer;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.config.ConfigFactory;
-import org.pac4j.http.client.direct.ParameterClient;
+import org.pac4j.http.client.direct.HeaderClient;
 import org.pac4j.jwt.config.signature.SecretSignatureConfiguration;
 import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
 
@@ -21,16 +21,14 @@ public class AuthConfigFactory implements ConfigFactory {
     @Override
     public Config build() {
         // REST authentication with JWT for a token passed in the url as the token parameter
-        ParameterClient parameterClient = new ParameterClient("token", new JwtAuthenticator(new SecretSignatureConfiguration(salt)));
-        parameterClient.setSupportGetRequest(true);
-        parameterClient.setSupportPostRequest(true);
+        HeaderClient client = new HeaderClient("Authorization", new JwtAuthenticator(new SecretSignatureConfiguration(salt)));
         // Define config
-        final Config config = new Config(parameterClient);
+        final Config config = new Config(client);
         // Register HttpActionAdapter to respond with proper HTTP response codes on auth errors
         config.setHttpActionAdapter(new HttpActionAdapter());
         // Set Authorizers to check of roles
         config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("admin"));
-        config.addAuthorizer("user", new RequireAnyRoleAuthorizer("user"));
+        config.addAuthorizer("user", new RequireAnyRoleAuthorizer("user", "admin"));
         return config;
     }
 
