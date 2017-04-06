3ebe8f35069e966143e189ed97db6cf867d687b0
Benjamin Bestmann <b.bestmann@gmail.com>
2017-04-06 16:01:19 +0200

    Token REST endpoint
    Implement REST endpoint POST /token for creating new tokens.

---
+++ openAPE/server/src/main/java/org/openape/server/rest/TokenRESTInterface.java
@@ -0,0 +1,32 @@
+package org.openape.server.rest;
+
+import org.jboss.resteasy.spi.UnauthorizedException;
+import org.openape.server.auth.AuthService;
+import spark.Spark;
+
+/**
+ * Rest Endpoint to get a token in exchange for a username and password.
+ */
+
+class TokenRESTInterface extends SuperRestInterface {
+
+    /**
+     * Register a new route POST /token to get a token in exchange for a username and password.
+     */
+
+    static void setupTokenRESTInterface() {
+        Spark.post("/token", (req, res) -> {
+            final String username = req.queryParams("username");
+            final String password = req.queryParams("password");
+            try {
+                // Try to get a token for the given username and password
+                return AuthService.getToken(username, password);
+            } catch(UnauthorizedException e) {
+                // If user could not get authorized, return status 401 (Unauthorized)
+                res.status(HTTP_STATUS_UNAUTHORIZED);
+                return e.getMessage();
+            }
+        });
+    }
+
+}
