4e68347957d8b7b7d05f3cff36fc3000c80036fd
Benjamin Bestmann <b.bestmann@gmail.com>
2017-04-10 22:25:19 +0200

    Anonymous Client
    Add a PAC4J client for anonymous profile authentication.

--- openAPE/server/src/main/java/org/openape/server/auth/AuthConfigFactory.java
+++ openAPE/server/src/main/java/org/openape/server/auth/AuthConfigFactory.java
@@ -1,6 +1,9 @@
 package org.openape.server.auth;
 
+import org.pac4j.core.authorization.authorizer.IsAnonymousAuthorizer;
 import org.pac4j.core.authorization.authorizer.RequireAnyRoleAuthorizer;
+import org.pac4j.core.client.Clients;
+import org.pac4j.core.client.direct.AnonymousClient;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.config.ConfigFactory;
 import org.pac4j.http.client.direct.HeaderClient;
@@ -21,14 +24,16 @@ public class AuthConfigFactory implements ConfigFactory {
     @Override
     public Config build() {
         // REST authentication with JWT for a token passed in the url as the token parameter
-        HeaderClient client = new HeaderClient("Authorization", new JwtAuthenticator(new SecretSignatureConfiguration(salt)));
+        HeaderClient headerClient = new HeaderClient("Authorization", new JwtAuthenticator(new SecretSignatureConfiguration(salt)));
+        AnonymousClient anonymousClient = new AnonymousClient();
         // Define config
-        final Config config = new Config(client);
+        final Clients clients = new Clients(headerClient, anonymousClient);
+        final Config config = new Config(clients);
         // Register HttpActionAdapter to respond with proper HTTP response codes on auth errors
         config.setHttpActionAdapter(new HttpActionAdapter());
         // Set Authorizers to check of roles
-        config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("admin"));
-        config.addAuthorizer("user", new RequireAnyRoleAuthorizer("user", "admin"));
+        config.addAuthorizer("adminOnly", new RequireAnyRoleAuthorizer("admin"));
+        config.addAuthorizer("userAndAdmin", new RequireAnyRoleAuthorizer("user", "admin"));
         return config;
     }
 
