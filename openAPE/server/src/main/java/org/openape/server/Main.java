35cd561940763fc6ba1f5c0d0894877fc902cf57
Benjamin Bestmann <b.bestmann@gmail.com>
2017-04-18 16:41:21 +0200

    EnsureIndexes on server startup
    Implement a parameter „ensureIndexes“ that allows to startup the server and force him to check that all required indexes for the application exist, or create them otherwise. Warning: Should not be used in production, because it can slow down application startup if collections are large.

--- openAPE/server/src/main/java/org/openape/server/Main.java
+++ openAPE/server/src/main/java/org/openape/server/Main.java
@@ -1,13 +1,22 @@
 package org.openape.server;
 
+import org.openape.server.database.mongoDB.DatabaseConnection;
 import org.openape.server.rest.SuperRestInterface;
+
+import java.util.Arrays;
 
 /**
  * Starting class of the project. Creates REST APIs.
  */
 public class Main {
     public static void main(String[] args) {
-        // Start rest api.
+        if(Arrays.asList(args).contains("ensureIndexes")) {
+            // Open database connection and make sure all indexes exist
+            System.out.println("Checking for indexes...");
+            DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+            databaseConnection.ensureIndexes();
+        }
+        // Start the REST API.
         new SuperRestInterface();
     }
 
