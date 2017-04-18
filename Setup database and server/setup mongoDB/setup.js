e9c53bd75791559bd0e98fc596368f2696017e60
Benjamin Bestmann <b.bestmann@gmail.com>
2017-04-18 16:23:47 +0200

    Add indexes on database setup
    Extend the database setup script in order to cerate unique indexes for the fields „username“ and „email“ on the users collection.

--- Setup database and server/setup mongoDB/setup.js
+++ Setup database and server/setup mongoDB/setup.js
@@ -1,8 +1,8 @@
 ﻿var connection = new Mongo("localhost:27017");
-var database = connection.getDB("openAPE");
+var database = connection.getDB("openAPE2");
 database.createUser({
-     user: "openAPE",
-     pwd: "<userPassword>",
+     user: "openAPE2",
+     pwd: "121212",
      roles: [ "readWrite", "dbAdmin" ]});
 database.createCollection("environment_contexts");
 database.createCollection("equipment_contexts");
@@ -14,3 +14,7 @@ database.createCollection("resources");
 database.createCollection("users");
 database.createCollection("test");
 database.test.insert( { x: 1 } );
+
+// Create indexes to make username and email unique fields in the users collection
+database.getCollection("users").createIndex({ "username": 1 }, { unique: true });
+database.getCollection("users").createIndex({ "email": 1 }, { unique: true });
