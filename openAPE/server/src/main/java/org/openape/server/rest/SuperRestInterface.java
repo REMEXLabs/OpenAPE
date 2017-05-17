ee2f382e807d621a45969b92c27343eb6586dbf5
Lukas84 <smirek@hdm-stuttgart.de>
2017-05-17 11:37:26 +0200

    modified SuperRestInterface.java, logging, static files, removed REST endpoint for resource manager

--- openAPE/server/src/main/java/org/openape/server/rest/SuperRestInterface.java
+++ openAPE/server/src/main/java/org/openape/server/rest/SuperRestInterface.java
@@ -1,8 +1,10 @@
 package org.openape.server.rest;
 
+import java.io.File;
 import java.io.IOException;
 
 import org.openape.api.Messages;
+import org.openape.server.Main;
 import org.openape.server.auth.AuthService;
 import org.openape.server.requestHandler.EnvironmentContextRequestHandler;
 import org.openape.server.requestHandler.EquipmentContextRequestHandler;
@@ -11,6 +13,8 @@ import org.openape.server.requestHandler.ResourceDescriptionRequestHandler;
 import org.openape.server.requestHandler.ResourceRequestHandler;
 import org.openape.server.requestHandler.TaskContextRequestHandler;
 import org.openape.server.requestHandler.UserContextRequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import spark.Request;
 import spark.Spark;
@@ -20,7 +24,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class SuperRestInterface {
-
+	static Logger logger = LoggerFactory.getLogger(SuperRestInterface.class	);
     public static final int HTTP_STATUS_OK = 200;
     public static final int HTTP_STATUS_CREATED = 201;
     public static final int HTTP_STATUS_NO_CONTENT = 204;
@@ -53,6 +57,25 @@ public class SuperRestInterface {
      */
     public SuperRestInterface() {
 
+    	logger.info("Setting up REST API");	
+
+        
+
+    	Spark.staticFiles.location("webcontent");
+        
+    	File extContent = new File(System.getProperty("java.io.tmpdir")+"/extContent");
+    	if (!extContent.exists())
+    	{ 
+    		extContent.mkdir();
+    	logger.info("created new folder:" + extContent.getAbsolutePath() );	
+    	} else {
+    		logger.info("Found folder:" + extContent.getAbsolutePath() );
+    	
+    	}
+    	Spark.staticFiles.externalLocation(System.getProperty("java.io.tmpdir")+"/extContent");
+   
+    	Spark.get("api", (req,res) -> new API()                                                                    );
+    	
         // AuthService singleton to enable security features on REST endpoints
         final AuthService authService = new AuthService();
 
@@ -62,23 +85,32 @@ public class SuperRestInterface {
         });
 
         // Test endpoint to see if server runs. Invoke locally: http://localhost:4567/hello
-        Spark.get(Messages.getString("SuperRestInterface.HelloWorldURL"), (req, res) -> Messages.getString("SuperRestInterface.HelloWorld")); //$NON-NLS-1$ //$NON-NLS-2$
+    	Spark.get(
+                Messages.getString("UserContextRESTInterface.HelloWorldURL"), (req, res) -> Messages.getString("UserContextRESTInterface.HelloWorld")); //$NON-NLS-1$ //$NON-NLS-2$
+
+        
+
+Spark.get(Messages.getString("SuperRestInterface.HelloWorldURL"), (req, res) -> Messages.getString("SuperRestInterface.HelloWorld")); //$NON-NLS-1$ //$NON-NLS-2$
         // Endpoint to receive tokens
         TokenRESTInterface.setupTokenRESTInterface(authService);
         ProfileRESTInterface.setupProfileRESTInterface();
+
         // Resource endpoints
         EnvironmentContextRESTInterface.setupEnvironmentContextRESTInterface(new EnvironmentContextRequestHandler(), authService);
         EquipmentContextRESTInterface.setupEquipmentContextRESTInterface(new EquipmentContextRequestHandler(), authService);
         ListingRESTInterface.setupListingRESTInterface(new ListingRequestHandler());
-        ResourceDescriptionRESTInterface.setupResourceDescriptionRESTInterface(new ResourceDescriptionRequestHandler(), authService);
-        ResourceManagerRESTInterface.setupResourceManagerRESTInterface();
+                
+ResourceDescriptionRESTInterface.setupResourceDescriptionRESTInterface(new ResourceDescriptionRequestHandler(), authService);
+        
         ResourceRESTInterface.setupResourceRESTInterface(new ResourceRequestHandler());
         TaskContextRESTInterface.setupTaskContextRESTInterface(new TaskContextRequestHandler(), authService);
         UserContextRESTInterface.setupUserContextRESTInterface(new UserContextRequestHandler(), authService);
+        logger.info("REST API successfully set up");
+
         // Test html interface found
         if(SuperRestInterface.TEST_ENVIRONMENT) {
             TestRESTInterface.setupTestRESTInterface();
-        }
+        }           
     }
 
 }
