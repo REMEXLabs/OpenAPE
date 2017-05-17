be23b18b91f258915827c92df1d738f83bfa4954
Lukas84 <smirek@hdm-stuttgart.de>
2017-05-17 10:29:41 +0200

    logging statements in Init.java

--- openAPE/server/src/main/java/org/openape/web/Init.java
+++ openAPE/server/src/main/java/org/openape/web/Init.java
@@ -1,13 +1,22 @@
 package org.openape.web;
 
+import org.openape.server.database.mongoDB.DatabaseConnection;
 import org.openape.server.rest.SuperRestInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import spark.servlet.SparkApplication;
 
 public class Init implements SparkApplication {
-
+Logger logger = LoggerFactory.getLogger(Init.class);
     @Override
     public void init() {
+    	logger.info("Initialising openAPE");
+    	logger.debug("Working directory: " +             System.getProperty("user.dir"));
+    	
+//    	start database connection for testing purposes
+    	DatabaseConnection.getInstance();
+    	
         // Start rest api.
         new SuperRestInterface();
     }
