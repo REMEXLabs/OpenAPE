35a99d5a9575bcf04b6bfe4e21d9109d06354d70
Lukas84 <smirek@hdm-stuttgart.de>
2017-07-11 11:17:48 +0200

    createUser added to ProfileHandler

--- openAPE/server/src/main/java/org/openape/server/requestHandler/ProfileHandler.java
+++ openAPE/server/src/main/java/org/openape/server/requestHandler/ProfileHandler.java
@@ -1,5 +1,32 @@
 package org.openape.server.requestHandler;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import org.openape.api.user.User;
+import org.openape.server.auth.PasswordEncoder;
+import org.openape.server.database.mongoDB.DatabaseConnection;
+import org.openape.server.database.mongoDB.MongoCollectionTypes;
+
 public class ProfileHandler {
+public 	static String createUser(User user) throws IOException, IllegalArgumentException {
+        final DatabaseConnection databaseconnection = DatabaseConnection.getInstance();
+        String id;
+        try {
+            String hashedPassword = PasswordEncoder.encode(user.getPassword());
+            user.setPassword(hashedPassword);
+            id = databaseconnection.storeData(MongoCollectionTypes.USERS, user);
+        } catch (final ClassCastException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new IOException(e.getMessage());
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+            throw new IOException(e.getMessage());
+        }
+        return id;
+    }
 
 }
