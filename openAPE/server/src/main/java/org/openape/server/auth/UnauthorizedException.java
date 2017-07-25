c587ce27e2f240ce843e17cf1110110411137533
su017 <su017@hdm-stuttgart.de>
2017-07-25 16:07:09 +0200

    fix: add ID to unauthorized exception

--- openAPE/server/src/main/java/org/openape/server/auth/UnauthorizedException.java
+++ openAPE/server/src/main/java/org/openape/server/auth/UnauthorizedException.java
@@ -4,7 +4,8 @@ package org.openape.server.auth;
  * Exception to be thrown if a user can not be authorized.
  */
 public class UnauthorizedException extends Exception {
-
+    private static final long serialVersionUID = -1987250205279214600L;
+    
     public UnauthorizedException() { super(); }
     public UnauthorizedException(String message) { super(message); }
     public UnauthorizedException(String message, Throwable cause) { super(message, cause); }
