ced36eb0e921dc9184d8b101978d7bd76b37c3d5
su017 <su017@hdm-stuttgart.de>
2017-07-26 11:17:44 +0200

    feature: complete resource object class

--- openAPE/API/src/main/java/org/openape/api/resourceDescription/ResouceObject.java
+++ openAPE/API/src/main/java/org/openape/api/resourceDescription/ResouceObject.java
@@ -1,5 +1,72 @@
 package org.openape.api.resourceDescription;
 
-public class ResouceObject {
+import java.io.File;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+import org.openape.api.DatabaseObject;
+import org.openape.api.Messages;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * Resource object describing resource files. Including the storage path and the
+ * owner id. It is stored into the database, when the corresponding resource is
+ * stored onto the file system.
+ */
+public class ResouceObject extends DatabaseObject {
+    private static final long serialVersionUID = -5926306380613419041L;
+
+    private String id = null;
+    private String fileName = null;
+    private String ownerId = null;
+    private ResourceDescription resourceDescription = null;
+
+    @XmlAttribute(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @XmlAttribute(name = "fileName")
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @XmlAttribute(name = "ownerID")
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    @XmlElement(name = "resourceDescription")
+    public ResourceDescription getResourceDescription() {
+        return resourceDescription;
+    }
+
+    public void setResourceDescription(ResourceDescription resourceDescription) {
+        this.resourceDescription = resourceDescription;
+    }
+
+    /**
+     * @return Storage path of the resource, including the filename.
+     */
+    @JsonIgnore
+    public String getPath() {
+        String resourceFolderPath = Messages.getString("ResourceList.rootFolder") + File.separator + Messages.getString("ResourceList.ResourceFolder"); //$NON-NLS-1$ //$NON-NLS-2$
+        return resourceFolderPath + File.separator + this.getOwnerId() + File.separator
+                + this.getFileName();
+    }
 
 }
