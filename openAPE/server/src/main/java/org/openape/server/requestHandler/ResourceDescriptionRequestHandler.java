d7387c92daf87acbc01bc1a5f09f7e46fe298273
su017 <su017@hdm-stuttgart.de>
2017-08-01 18:11:36 +0200

    Fix: add check if resource descriptions have a valid resoruce.

--- openAPE/server/src/main/java/org/openape/server/requestHandler/ResourceDescriptionRequestHandler.java
+++ openAPE/server/src/main/java/org/openape/server/requestHandler/ResourceDescriptionRequestHandler.java
@@ -1,9 +1,11 @@
 package org.openape.server.requestHandler;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import org.openape.api.DatabaseObject;
 import org.openape.api.Messages;
+import org.openape.api.Property;
 import org.openape.api.listing.Listing;
 import org.openape.api.resourceDescription.ResourceDescription;
 import org.openape.server.database.mongoDB.DatabaseConnection;
@@ -19,6 +21,16 @@ import org.openape.server.rest.ResourceRESTInterface;
 public class ResourceDescriptionRequestHandler {
 
     private static final MongoCollectionTypes COLLECTIONTOUSE = MongoCollectionTypes.RESOURCEDESCRIPTION;
+    private static final String ResourceURIPropertyName = "resource-uri";
+    private static final String ResourceURIPattern = "https://res\\.openurc\\.org/api/resources/[a-zA-Z\\d]*";
+    private static final String ResourceURIPatternPrefix = "https://res\\.openurc\\.org/api/resources/";
+    private static final String WrongURIFormatMsg = "The property with the name 'resource-uri'"
+            + " has to have a value with the pattern 'https://res.openurc.org/api/resources/<resourceID>'";
+    private static final String NoResoruceWithThatID = "There is no resource for your resource"
+            + " description. Your given resource id was: ";
+    private static final String PropertyMissing = "Resource description must contain a property"
+            + " with the name 'resource-uri' and a value with the pattern"
+            + " 'https://res.openurc.org/api/resources/<resourceID>'";
 
     /**
      * Method to store a new resource description into the server. It is used by
@@ -33,8 +45,10 @@ public class ResourceDescriptionRequestHandler {
      * @throws IllegalArgumentException
      *             if the parameter is not a complete environment context.
      */
-    public String createResourceDescription(Object resourceDescription) throws IOException,
-            IllegalArgumentException {
+    public String createResourceDescription(ResourceDescription resourceDescription)
+            throws IOException, IllegalArgumentException {
+        // Check if the resource description has a valid resource.
+        this.hasDescriptionResource(resourceDescription);
         // get database connection.
         final DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
         // try to store data. Class cast exceptions will be thrown as illegal
@@ -42,7 +56,7 @@ public class ResourceDescriptionRequestHandler {
         String id = null;
         try {
             id = databaseConnection.storeData(ResourceDescriptionRequestHandler.COLLECTIONTOUSE,
-                    (DatabaseObject) resourceDescription);
+                    resourceDescription);
         } catch (final ClassCastException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
@@ -134,6 +148,46 @@ public class ResourceDescriptionRequestHandler {
     }
 
     /**
+     * Checks if a resource with the ID given in the resource description is
+     * available.
+     *
+     * @param resourceDescription
+     * @throws IllegalArgumentException
+     *             if it has no property with the name resource-uri, it has the
+     *             wrong form, or no corresponding resource was found.
+     */
+    private void hasDescriptionResource(ResourceDescription resourceDescription)
+            throws IllegalArgumentException, IOException {
+        // Search for a property with the right name to contain the resource ID.
+        boolean propertyFound = false;
+        for (final Property property : resourceDescription.getPropertys()) {
+            if (property.getName()
+                    .equals(ResourceDescriptionRequestHandler.ResourceURIPropertyName)) {
+                propertyFound = true;
+                // If a property with the right name is found check if value has
+                // the right pattern.
+                if (!Pattern.matches(ResourceDescriptionRequestHandler.ResourceURIPattern,
+                        property.getValue())) {
+                    throw new IllegalArgumentException(
+                            ResourceDescriptionRequestHandler.WrongURIFormatMsg);
+                }
+                // Check if database contains corresponding resource object.
+                final String resourceID = property.getValue().replaceAll(
+                        ResourceDescriptionRequestHandler.ResourceURIPatternPrefix, "");
+                final DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
+                if (null == databaseConnection.getData(MongoCollectionTypes.RESOURCEOBJECTS,
+                        resourceID)) {
+                    throw new IllegalArgumentException(
+                            ResourceDescriptionRequestHandler.NoResoruceWithThatID + resourceID);
+                }
+            }
+        }
+        if (!propertyFound) {
+            throw new IllegalArgumentException(ResourceDescriptionRequestHandler.PropertyMissing);
+        }
+    }
+
+    /**
      * Method to update an existing resource description on the server. It is
      * used by the rest API {@link ResourceDescriptionRESTInterface} and uses
      * the server database {@link DatabaseConnection}.
@@ -149,8 +203,10 @@ public class ResourceDescriptionRequestHandler {
      *             if the id is no valid id, not assigned or the environment
      *             context is not valid.
      */
-    public boolean updateResourceDescriptionById(String id, Object resourceDescription)
+    public boolean updateResourceDescriptionById(String id, ResourceDescription resourceDescription)
             throws IOException, IllegalArgumentException {
+        // Check if the resource description has a valid resource.
+        this.hasDescriptionResource(resourceDescription);
         // get database connection.
         final DatabaseConnection databaseConnection = DatabaseConnection.getInstance();
 
@@ -160,8 +216,7 @@ public class ResourceDescriptionRequestHandler {
         boolean success;
         try {
             success = databaseConnection.updateData(
-                    ResourceDescriptionRequestHandler.COLLECTIONTOUSE,
-                    (DatabaseObject) resourceDescription, id);
+                    ResourceDescriptionRequestHandler.COLLECTIONTOUSE, resourceDescription, id);
         } catch (final ClassCastException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
