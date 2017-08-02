039e6edcf81e36eb745b779e6a4f98a183af3139
Lukas84 <smirek@hdm-stuttgart.de>
2017-08-02 15:06:35 +0200

    adminTest added

---
+++ openAPE/client/src/test/java/org/openape/client/AdminTest.java
@@ -0,0 +1,7 @@
+package org.openape.client;
+
+public class AdminTest {
+public static void main(String[] args) {
+	OpenAPEClient client = new OpenAPEClient("admin", "admin", "http://Lukas_Arbeit:4567");
+}
+}
