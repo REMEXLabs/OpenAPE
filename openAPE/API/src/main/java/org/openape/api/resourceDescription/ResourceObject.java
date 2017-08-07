94f5ea1ed09d5ea4b9dbfaf864526929a99abb91
su017 <su017@hdm-stuttgart.de>
2017-08-07 02:12:38 +0200

    feature: add group acces right field to resource object

--- openAPE/API/src/main/java/org/openape/api/resourceDescription/ResourceObject.java
+++ openAPE/API/src/main/java/org/openape/api/resourceDescription/ResourceObject.java
@@ -9,6 +9,7 @@ import javax.xml.bind.annotation.XmlElement;
 
 import org.openape.api.Messages;
 import org.openape.api.Resource;
+import org.openape.api.group.GroupAccessRight;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -24,17 +25,20 @@ public class ResourceObject extends Resource {
     private String fileName = null;
     private String ownerId = null;
     private String mimeType = null;
+    private GroupAccessRight groupAccessRight = null;
     private List<ResourceDescription> resourceDescriptions = null;
 
     public ResourceObject() {
         this.resourceDescriptions = new ArrayList<ResourceDescription>();
     }
-    
-    public ResourceObject(final String fileName, final String ownerId, final String mimeType) {
+
+    public ResourceObject(final String fileName, final String ownerId, final String mimeType,
+            final GroupAccessRight groupAccessRight) {
         super();
         this.fileName = fileName;
         this.ownerId = ownerId;
         this.mimeType = mimeType;
+        this.groupAccessRight = groupAccessRight;
     }
 
     @JsonIgnore
@@ -45,6 +49,20 @@ public class ResourceObject extends Resource {
     @XmlAttribute(name = "fileName")
     public String getFileName() {
         return this.fileName;
+    }
+
+    /**
+     * @return Storage path of the resource, excluding the filename.
+     */
+    @JsonIgnore
+    public String getFolder() {
+        final String resourceFolderPath = Messages.getString("ResourceList.rootFolder") + File.separator + Messages.getString("ResourceList.ResourceFolder"); //$NON-NLS-1$ //$NON-NLS-2$
+        return resourceFolderPath + File.separator + this.getOwnerId();
+    }
+
+    @XmlElement(name = "groupAccessRight")
+    public GroupAccessRight getGroupAccessRight() {
+        return this.groupAccessRight;
     }
 
     @XmlAttribute(name = "id")
@@ -71,15 +89,6 @@ public class ResourceObject extends Resource {
         return resourceFolderPath + File.separator + this.getOwnerId() + File.separator
                 + this.getFileName();
     }
-    
-    /**
-     * @return Storage path of the resource, excluding the filename.
-     */
-    @JsonIgnore
-    public String getFolder() {
-        final String resourceFolderPath = Messages.getString("ResourceList.rootFolder") + File.separator + Messages.getString("ResourceList.ResourceFolder"); //$NON-NLS-1$ //$NON-NLS-2$
-        return resourceFolderPath + File.separator + this.getOwnerId();
-    }
 
     @XmlElement(name = "resourceDescription")
     public List<ResourceDescription> getResourceDescriptions() {
@@ -88,6 +97,10 @@ public class ResourceObject extends Resource {
 
     public void setFileName(final String fileName) {
         this.fileName = fileName;
+    }
+
+    public void setGroupAccessRight(final GroupAccessRight groupAccessRight) {
+        this.groupAccessRight = groupAccessRight;
     }
 
     public void setId(final String id) {
