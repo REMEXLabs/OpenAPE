3347df2274f81297e0af577a2dd13ab44ad0dd79
jaufmann <wjaufmann@gmx.de>
2017-08-09 17:48:08 +0200

    refactoring

--- openAPE/server/src/main/java/org/openape/ui/velocity/Footer.java
+++
@@ -1,9 +0,0 @@
-package org.openape.ui.velocity;
-
-public class Footer {
-    public String generateFooter() {
-        final String footerContent = "Hochschuel der Medien 2017 <br> <a href='http://localhost:4567/legalNotice'>Legal Notice</a>";
-        return footerContent;
-    }
-
-}
