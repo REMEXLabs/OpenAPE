f41ce88c1f60f0dde7461072ad63a21ac3edd1e3
su017 <su017@hdm-stuttgart.de>
2017-08-16 07:57:21 +0200

    Feature: add equipmentcontext test

---
+++ openAPE/server/src/test/java/org/openape/api/taskcontext/TestEquipmentContext.java
@@ -0,0 +1,34 @@
+package org.openape.api.taskcontext;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openape.api.Property;
+import org.openape.api.equipmentcontext.EquipmentContext;
+
+public class TestEquipmentContext {
+    public static EquipmentContext sampleEquipmentContext() {
+        EquipmentContext equipmentContext = new EquipmentContext();
+        Property prop1 = new Property("http://openurc.org/ns/res#friendlyName","My iPad");
+        equipmentContext.addProperty(prop1);
+        prop1.addDescriptor("http://www.w3.org/XML/1998/namespace/lang", "en");
+        Property prop2 = new Property("http://openurc.org/ns/res#friendlyName", "Mein iPad");
+        equipmentContext.addProperty(prop2);
+        prop2.addDescriptor("http://www.w3.org/XML/1998/namespace/lang", "de");
+        Property prop3 = new Property("http://openurc.org/ns/res#devicePlatform", "iOS");
+        equipmentContext.addProperty(prop3);
+        Property prop4 = new Property("http://openurc.org/ns/res#deviceType", "iPad-3gen");
+        equipmentContext.addProperty(prop4);
+        Property prop5 = new Property("http://openurc.org/ns/res#resolution", "1536x2048");
+        equipmentContext.addProperty(prop5);
+        return equipmentContext;
+    }
+    
+    @Test
+    public void testEquals() {
+        Assert.assertTrue(sampleEquipmentContext().equals(sampleEquipmentContext()));
+    }
+    
+    
+    
+
+}
