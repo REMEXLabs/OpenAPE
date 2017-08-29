e07d2a48be69efbbebf822e5a171bd5741a4e3ec
Tobias Ableitner <tobias.ableitner@t-online.de>
2017-08-29 13:22:37 +0200

    - create JUnit class for ResourceAuthService

---
+++ openAPE/server/src/test/java/org/openape/server/auth/TestResourceAuthService.java
@@ -0,0 +1,24 @@
+package org.openape.server.auth;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestResourceAuthService {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
