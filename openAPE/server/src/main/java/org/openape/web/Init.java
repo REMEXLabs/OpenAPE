68ae071030e3d31e1a7535d47bc5ac06c0c17da9
su017 <su017@hdm-stuttgart.de>
2017-11-12 16:51:14 +0100

    Fix: admin gets created in war

--- openAPE/server/src/main/java/org/openape/web/Init.java
+++ openAPE/server/src/main/java/org/openape/web/Init.java
@@ -1,5 +1,6 @@
 package org.openape.web;
 
+import org.openape.server.admin.AdminInterface;
 import org.openape.server.database.mongoDB.DatabaseConnection;
 import org.openape.server.rest.SuperRestInterface;
 import org.slf4j.Logger;
@@ -17,6 +18,14 @@ public class Init implements SparkApplication {
 
         // start database connection for testing purposes
         DatabaseConnection.getInstance();
+        
+        // create a standard Admin user
+        try {
+            AdminInterface.createAdmin();
+        } catch (final Exception e) {
+            this.logger.error("Admin could not be created or no Admin found. System could not start.");
+            return;
+        }
 
         // Start rest api.
         new SuperRestInterface();
