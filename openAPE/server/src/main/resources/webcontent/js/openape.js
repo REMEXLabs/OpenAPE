d308d918c33087acad76f7ba8f9787b888c4ab3f
su017 <su017@hdm-stuttgart.de>
2018-01-29 17:43:36 +0100

    fix: email varification

--- openAPE/server/src/main/resources/webcontent/js/openape.js
+++ openAPE/server/src/main/resources/webcontent/js/openape.js
@@ -827,7 +827,9 @@
 		   return objAjaxParameters
 	   }
     
-	   function validateEmail(email) {	        
+	   function validateEmail(email) {	    
+	        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	        return re.test(email);
 	   }
 	
 	    
