68b9574b12295147f8ea1c8b25e7b582cfe29bf7
Lukas84 <smirek@hdm-stuttgart.de>
2018-02-20 09:30:10 +0100

    adde openapeTest.js

---
+++ openAPE/API/iso_24752_examples.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+var openape_examples = {
+		getExample1 : function() {
+			return '{
+	  "default": {
+	    "preferences": 
+	      { "http://terms.gpii.net/speech-output": true }
+	  }
+	}
+	';
+		}	
+	
+};	
+
+		
+		
+
+var iso_24752_examples = {
+		
+};
