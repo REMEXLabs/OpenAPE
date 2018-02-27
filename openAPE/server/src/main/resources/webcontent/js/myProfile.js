de9b0fbcffdb174ec75a005f56fa09ecae45e75f
su017 <su017@hdm-stuttgart.de>
2018-02-27 22:01:55 +0100

    fix: user null

--- openAPE/server/src/main/resources/webcontent/js/myProfile.js
+++ openAPE/server/src/main/resources/webcontent/js/myProfile.js
@@ -1,19 +1,22 @@
-$(document).ready(function() {
+$(document).ready(
+		function() {
+			var userdata = JSON.parse(openape.getUser(localStorage
+					.getItem("token")).responseText);
+			localStorage.setItem("username", userdata.username);
+			localStorage.setItem("userid", userdata.id);
+			if (userdata.roles.includes("admin")) {
+				localStorage.setItem("role", "admin");
+			}
 
-	var userdata = JSON.parse(openape.getUser(localStorage.getItem("token")).responseText);
-	localStorage.setItem("username", userdata.username);
-	localStorage.setItem("userid", userdata.id);
-	if(userdata.roles.includes("admin")){
-		localStorage.setItem("role", "admin");
-	}
-})
-
-$.("btnChangePassword").click(function(){
-	let pwOld = $.("iptPasswordOld").getValue();
-	let pw1 = $.("iptPassword1").getValue();
-	let pw2 = $.("iptPassword2").getValue();
-	if (pw1 == pw2) {
-		
-		window.openape.changePassword(pwOld, pw1)
-	}
-});
+			$(".btnChangePassword").click(function() {
+				let
+				pwOld = $(".iptPasswordOld").getValue();
+				let
+				pw1 = $(".iptPassword1").getValue();
+				let
+				pw2 = $(".iptPassword2").getValue();
+				if (pw1 == pw2) {
+					window.openape.changePassword(pwOld, pw1);
+				}
+			});
+		});
