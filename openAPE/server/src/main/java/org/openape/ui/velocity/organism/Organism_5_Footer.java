92be62ea5399598274c40a874241a28dbfb55f3c
Sebastian Koch <sebastian@0py.de>
2018-07-06 13:40:51 +0200

    Changes wording in footer

--- openAPE/server/src/main/java/org/openape/ui/velocity/organism/Organism_5_Footer.java
+++ openAPE/server/src/main/java/org/openape/ui/velocity/organism/Organism_5_Footer.java
@@ -3,7 +3,7 @@ package org.openape.ui.velocity.organism;
 public class Organism_5_Footer {
     public String generateFooter() {
         final String footerContent = "\u00a9 Hochschule der Medien / Stuttgart Media University 2017 <br>"
-        		+ " <a href=' https://gpii.eu/legal/en/privacy.html'>Privacy</a> <br>"
+        		+ " <a href=' https://gpii.eu/legal/en/privacy.html'>Privacy Policy</a> <br>"
         		+ " <a href='https://gpii.eu/legal/en/imprint.html'>Imprint</a> "
         		;
         return footerContent;
