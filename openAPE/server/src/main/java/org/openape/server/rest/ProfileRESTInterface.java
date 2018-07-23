ab085c6b82ac3e30a3809c5ae166f97a91b11c20
Lukas84 <smirek@hdm-stuttgart.de>
2018-07-23 10:20:36 +0200

    Endpoint MyId returns the user's Id, correct filter is set

--- openAPE/server/src/main/java/org/openape/server/rest/ProfileRESTInterface.java
+++ openAPE/server/src/main/java/org/openape/server/rest/ProfileRESTInterface.java
@@ -89,10 +89,10 @@ public class ProfileRESTInterface extends SuperRestInterface {
                     }
                 });
 
-        Spark.before(OpenAPEEndPoints.MY_ID, authService.authorize("user"));
+        Spark.before(OpenAPEEndPoints.MY_ID, authService.authorize("anonymous"));
         
         Spark.get(OpenAPEEndPoints.MY_ID, (req, res) -> {
-            SuperRestInterface.logger.info("blablubber");
+            
             logger.debug("lusm: "  + req.headers("authorization"));
             final String id =  authService.getAuthenticatedUser(req, res).getId();
             SuperRestInterface.logger.info("id: " + id);
